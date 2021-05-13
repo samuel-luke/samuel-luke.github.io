369e7b7c3c2d879177947630e635eb06ad969a81
Samuel Luke <samuel.luke@aggiemail.usu.edu>
2021-05-13 16:55:27 -0600

    Added age

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,11 @@
 //         $("nav").css("height", "25vh");
 //     }
 // }
+
+// Calculate Age
+$(document).ready(function() {
+    dob = new Date("1999-02-16");
+    var today = new Date();
+    var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000));
+    $('#age').html(age);
+})
